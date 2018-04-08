4e0d21385f396362f5c9cfdc2aa4f69383010434
tosh2 <josuetz21@gmail.com>
2018-04-07 21:05:56 -0600

    tosh - version 5.0, bootstrap añadido, cambios wilder

---
+++ src/java/Controladores/Ctrl_veroferta.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import Modelos.Modelo_Oferta;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author tosh
+ */
+public class Ctrl_veroferta extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            RequestDispatcher rd = null;
+            
+            Modelo_Oferta ofertaModel = new Modelo_Oferta();
+            
+            int resultado = 0;  /*  tosh - Verifica si CRUD se ejecuto correctamente: 0 = error */
+            
+                        /*  obtenemos Correo/Usuario actual */
+            String usuario = (String)request.getSession().getAttribute("usuario");
+            
+            if(request.getParameter("btn_postularse")    !=  null){
+                
+                String idOferta = request.getParameter("txt_idOferta");
+                
+                        /*  Creamos la oferta **verificar si es correcta o no   */
+                resultado = ofertaModel.postularse(usuario, idOferta);
+                rd = request.getRequestDispatcher("verOferta.jsp?idOferta="+idOferta);
+            }
+            
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
